52b1dcab31a925a2e829012430099087a22a76aa
Ludovic16513 <56023526+Ludovic16513@users.noreply.github.com>
2020-01-13 09:40:44 +0100

    Etape 1

--- parts/footer.php
+++ parts/footer.php
@@ -1,1 +1,14 @@
 put some footer here
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<script src="../public/app.js"></script>
+</body>
+</html>
--- parts/header.php
+++ parts/header.php
@@ -1,1 +1,14 @@
 put some header here
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+include 'menu.php'
+?>
+</body>
+</html>
